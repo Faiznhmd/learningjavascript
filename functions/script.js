e2cc0dfc024e381c08abd6c49ce7599c5bab8971
Faiznhmd <fiaznhmd.0624@gmail.com>
2023-07-13 18:44:01 +0530

    how function is work

--- functions/script.js
+++ functions/script.js
@@ -48,14 +48,17 @@
 // but console bhar rhga tu varable ko catch nai  kr payega
 //  example---
 
-// correct
+//-------- variable scope--------
+// -----correct----
+
 // const filename = "xyz.name";
 // function download() {
 //   console.log(filename);
 // }
 // download();
 
-// nor correct
+// ------not correct-------
+
 // function download() {
 //   const filename = "xyz.name";
 // }
@@ -65,20 +68,89 @@
 // -----function expression and function declaration-------
 
 // function declaration
+
 // function login() {
 
 // }
 
 // expression
+
 // const login = function () {
 //   console.log("faizan");
 // };
 // login();
 
-//  --------------callback---------------
+//  --------------callback---------- function pass krte as a parameter(freedom deta hai user to apko text jaisa rkhna hai rkho)
 
-function formatText(text) {
-  return text.toUpperCase();
+// function format(text) {
+//   return text.toUpperCase();
+// }
+// const result = format("hello");
+// console.log(result);
+
+// function formatText(text, formatCb) {
+//   return typeof formatCb === "functoin" ? formatCb(text) : text.toUpperCase();
+// }
+// const result = formatText("faizan", function (text) {
+//   return text.charAt(0).toUpperCase + text.slice(1);
+// });
+// console.log(result);
+
+// -----IIFE(imediately invoked function expresion)-------
+
+// (function aspire() {
+//   console.log("setUpDone");
+// })();
+
+// ----------Arrow function--------
+
+// const login = () => {
+//   console.log("hii there");
+// };
+// login();
+
+// const sum = (num1, num2) => {
+//   return num1 + num2;
+// };
+// const result = sum(4, 5);
+// console.log(result);
+
+// -------------How function work behind the scene--------
+// same variable name so js beahve
+
+var x = 1;
+a();
+b();
+console.log(x);
+
+function a() {
+  var x = 10;
+  console.log(x);
 }
-const result = formatText("hello");
-console.log(result);
+
+function b() {
+  var x = 100;
+  console.log(x);
+}
+// when the control is taken in 123 line now the function is executed
+
+// when javascript run a global gobalexecution context is created
+
+// and GEC have 2 components memory and code components
+
+// memory component is also called variable environment
+
+// now before execution a single line of code memory is pushed in a first phase memory phase
+
+// memory allocated to x a(), b() and all of this contain undefined in a first phase and a , b allocated to a exact function code
+
+// when we run a first line of code undefined change into 1
+
+// when function a is invoked and a brand new executionm
+
+// context is ceated  another  execution context is
+// created and separate manner funciton x has separate excution context mini place enviroment where this small function is excuted
+
+// when we console a program console find a value in his ,local memory space and then he print the value on his console
+
+// then whole excecution context is vanished the memory is gone and the cal stack is also gone
