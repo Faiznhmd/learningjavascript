009fb0674f06094c075dd9349e3e50af6ab3af62
Faiznhmd <fiaznhmd.0624@gmail.com>
2023-07-14 18:44:49 +0530

    Datatypes

---
+++ Datatypes/script.js
@@ -0,0 +1,34 @@
+// There are total 9 types of Datatypes
+
+// 1.Primitive Datatypes( Only Single Value Stored no ,string,  boolean)
+
+// 1.1 Number(All types of  Number)
+
+// let age = 25;
+// let temp = -36;
+// let price = 65.7;
+
+// 1.2 String(Textual Data)
+
+//  let name= Faizan
+
+// 1.3 Boolean( True or False)
+
+// 1.4 undefined(Declare but not giving a vlaue)
+
+// let age;
+// console.log(age);
+
+// 1.4 BigInt( 2**53-1)
+
+// 2.Structural Type
+
+// a.Object
+// 1.1 function
+// 1.2 Arrays(collection of value)
+// 1.3 Maps
+// 1.4 Sets
+// 1.5Date
+
+// 3. Structural Root
+// 1.1 Null
