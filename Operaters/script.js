574eaa3565a52aa5aaa5dca60b88e4c1a31abcb0
Faiznhmd <fiaznhmd.0624@gmail.com>
2023-07-15 14:51:52 +0530

    operaters

---
+++ Operaters/script.js
@@ -0,0 +1,123 @@
+// There are 10 types of operaters
+
+//  1.Assignmnet operaters
+// let x = 10;
+// let y;
+// y = x;
+// console.log(y);
+
+// 1.2 Compound operaters
+
+// let x = 20;
+// let y = 10;
+// x += y;
+// x -= y;
+// x *= y;
+// console.log(x);
+
+// 1.3 Arithmetic Operaters
+
+// let x = 10;
+// x += 1;
+// x++;
+// x--;
+// console.log(x);
+
+// 1.4 Logical Operaters(For booleans)
+
+// 1.Logical AND &&
+
+// (both side true then value also gets True and once is false so the value is false )
+
+// let happy = true && true;
+// console.log(happy);
+
+// let sad = true && false;
+// console.log(sad);
+
+// const isLoggedIn = true;
+// const hasPermision = true;
+// if (isLoggedIn && hasPermision) {
+//   console.log("Delete an succesfully");
+// }
+
+// const isLoggedIn = true;
+// const hasPermision = false;
+// if (isLoggedIn && hasPermision) {
+//   console.log("Delete an succesfully");
+// }
+
+// just for knowledge
+
+// cat or dog = dog(right wala)
+// cat or false= false
+
+// 2. logical or || (only one side ture then  output is also true)
+
+// const happy = true || false;
+// console.log(happy);
+
+// const isLoggedIn = true;
+// const hasPermision = false;
+// if (isLoggedIn || hasPermision) {
+//   console.log("Delete an succesfully");
+// }
+
+// 3. logical not ! ( vice versa ,true se false , false se true)
+
+// const happy = true;
+// console.log(!happy);
+
+// const sad = false;
+// console.log(!sad);
+
+// string par false krta hai logical not
+
+// const animal = "cat";
+// console.log(!animal);
+
+// const isLoggedIn = false;
+// if (!isLoggedIn) {
+//   console.log("Redirect to login page");
+// }
+
+// const isLogged = true;
+// if (!isLogged) {
+//   console.log("Redirect to login page");
+// }
+
+// 1.5 Conditional operaters
+
+// const userRole = "admin";
+// if (userRole === "admin") {
+//   console.log("you are an admin");
+// } else {
+//   console.log("youo are not an amdin");
+// }
+
+// -----------Ternary ooperater-------------
+
+// condition ? '' : '';\
+
+// const userRole = "admin";
+// userRole === "admin" ? console.log("faizan") : console.log("ahmad");
+
+// ----comparison operaters-----( always give boolean)
+// 1. Equal(==)
+
+// console.log(4 == 4);
+// console.log(4 == 5);
+
+// 2. not equal(!=)
+
+// console.log(8 != 9);
+
+// console.log(3 != 2);
+// console.log(2 != 2);
+
+// 3. strict equal
+
+// console.log(5 === 5);
+// console.log(5 === "5");
+
+// opearter precedence use for calculation in this type pf question
