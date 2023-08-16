6d0dbed9dad10d85a6cfc7b1df20adb4184ff682
Faiznhmd <fiaznhmd.0624@gmail.com>
2023-08-16 20:07:30 +0530

    spread done

---
+++ Rest and Spread/script.js
@@ -0,0 +1,62 @@
+// Rest Spread denote as ...
+
+// spread means unpack elements
+
+//  spread=elemnents ko copy kr skte hai
+
+// copy--------
+
+// const numbers = [1, 2, 3];
+// const newNumbers = [...numbers];
+// console.log(newNumbers);
+
+//  two arrays merge-------(sath mai)
+
+// const oldnumbers = [1, 2, 3];
+// const newnumbers = [4, 5, 6];
+// console.log([...oldnumbers, ...newnumbers]);
+
+// comspose-----------
+
+// const oldnumbers = [1, 2, 3];
+// const newnumbers = [4, 5, 6];
+// console.log([200, ...oldnumbers, ...newnumbers, 300]);
+
+// string------------
+
+// const name = "FaizanAhmad";
+// console.log([...name]);
+
+// objects------------
+
+// const order = {
+//   product: "dove",
+//   date: "16/8/23",
+//   customerName: "faizan",
+// };
+
+// const neworder = {
+//   ...order,
+// };
+// console.log(neworder);
+
+// merge 2 object------------
+
+// const user = {
+//   name: "faizan",
+//   age: 19,
+//   birth: 24 / 6 / 2003,
+// };
+
+// const address = {
+//   address: "jammu",
+// };
+
+// const newuser = {
+//   ...user,
+//   ...address,
+// };
+// console.log(newuser);
+
+
+// Rest opearter--------------
